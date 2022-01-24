fa3123f70e816a400cfc0bc8dfd0fa161acf539a
Penny Lam <pennylam@live.com>
2022-01-24 17:41:37 +0900

    fix

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,11 +1,6 @@
 import axios from "axios";
 
-
 const BASE_URL = "https://ecomm-styleup.herokuapp.com/api/";
-
-export const axiosInstance = axios.create({
-    baseURL: "https://ecomm-styleup.herokuapp.com/api/"
-})
 
 const TOKEN = 
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZDNhZWIyM2M1YWM2MTUxY2E2ODA5YiIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2NDEzNTYwNTcsImV4cCI6MTY0MTYxNTI1N30.-Rj3ZfRqKLfuqByT88qULyd5QZQYb__idP34GZA7jk8"
